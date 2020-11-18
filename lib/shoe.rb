c5ea4bad42ef117974f4cd250d7c4b9bbdf4e308
Conor Murnane <conormurnane11@gmail.com>
2020-11-17 20:48:02 -0500

    Shoe Lab class CONSTANTS

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,9 +1,18 @@
+require 'pry'
 class Shoe
   attr_accessor :color, :size, :material, :condition
   attr_reader :brand
 
+  BRANDS = []
+
   def initialize(brand)
     @brand = brand
+    #binding.pry
+    if BRANDS.include?(brand)
+    else 
+    BRANDS << brand
+    #binding.pry
+    end
   end
 
   def cobble
